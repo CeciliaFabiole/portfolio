782c2607dd5daa8b13f88ce4c19b05531be12ac2
ceci <ceciliafabiole18@gmail.com>
2023-01-19 18:12:45 +0100

    fix: warnings

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export function ErrorPage() {
-	const navigate = useNavigate();
 	return (
 		<div>
 			<div className="fullScreen-section text-light">
