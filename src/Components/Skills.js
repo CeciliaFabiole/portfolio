14be73848442f5d078cdc9fea4491138ee32598e
ceci <ceciliafabiole18@gmail.com>
2023-01-21 16:55:42 +0100

    fix: fix responsive details

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,15 +4,20 @@ import indesignLine from "../assets/indesign-line.svg"
 import lightroomLine from "../assets/lightroom-line.svg"
 
 function Skills() {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     return (
         <div className="container-fluid">
             <div className="container d-flex flex-column">
                 <div className="desktop-about-query">
                     <div className='contenitore-img d-flex align-items-end' id='skill-color'>
                         <div className='container'>
-                        <h1 className="spontaneous-img">{t("skill_copertina")}</h1>
+                            <h1 className="spontaneous-img">{t("skill_copertina")}</h1>
                         </div>
+                    </div>
+
+                    <div className='contenitore-titolo'>
+                        <h4>{t("skill_program")}</h4>
+                        <hr className='hr-title'></hr>
                     </div>
                     <div className="griglia">
                         <SkillsCard logo='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-original.svg' title="HTML5" paragraph={t("skills_1y")} />
@@ -26,13 +31,26 @@ function Skills() {
                         <SkillsCard logo="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/postgresql/postgresql-original.svg" title="PostgresQL" paragraph={t("skills_1y")} />
                         <SkillsCard logo="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/bootstrap/bootstrap-original.svg" title="Bootstrap" paragraph={t("skills_1y")} />
                         <SkillsCard logo="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/git/git-original.svg" title="Git" paragraph={t("skills_1y")} />
+                    </div>
+                    <div className='contenitore-titolo'>
+                        <h4>{t("skill_adobe")}</h4>
+                        <hr className='hr-title'></hr>
+                    </div>
+                    <div className="griglia">
                         <SkillsCard logo="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/illustrator/illustrator-line.svg" title="Illustrator" paragraph={t("skills_3y")} />
                         <SkillsCard logo={indesignLine} title="InDesign" paragraph={t("skills_3y")} />
                         <SkillsCard logo="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/photoshop/photoshop-line.svg" title="Photoshop" paragraph={t("skills_3y")} />
                         <SkillsCard logo={lightroomLine} title="Lightroom" paragraph={t("skills_3y")} />
                         <SkillsCard logo="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/xd/xd-line.svg" title="XD" paragraph={t("skills_3y")} />
+                    </div>
+                    <div className='contenitore-titolo'>
+                        <h4>{t("skill_languages")}</h4>
+                        <hr className='hr-title'></hr>
+                    </div>
+                    <div className="griglia">
+                        <SkillsCard logo="https://cdn-icons-png.flaticon.com/512/5315/5315858.png" title={t("italian")} paragraph={t("mothertongue")} />
                         <SkillsCard logo="https://cdn-icons-png.flaticon.com/512/197/197374.png" title={t("english")} paragraph="B2" />
-                        <SkillsCard logo="https://cdn-icons-png.flaticon.com/512/197/197560.png" title={t("french")}paragraph="C1" />
+                        <SkillsCard logo="https://cdn-icons-png.flaticon.com/512/197/197560.png" title={t("french")} paragraph="C1" />
                         <SkillsCard logo="https://cdn-icons-png.flaticon.com/512/197/197571.png" title={t("german")} paragraph="B2" />
                         <SkillsCard logo="https://cdn-icons-png.flaticon.com/512/197/197566.png" title={t("greek")} paragraph={t("work_in_progress")} />
                     </div>
